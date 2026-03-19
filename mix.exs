92a033478b1d51cc09eeec1041893c80f5b088ec
kambiwa <mwembo6@gmail.com>
2026-03-19 16:04:02 +0200

    Add PDF generator dependency and include audio file for uploads

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule EnrouteHaye.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
+      {:pdf_generator, "~> 0.6"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
